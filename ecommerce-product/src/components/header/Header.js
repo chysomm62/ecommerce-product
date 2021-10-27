fce46e17102ec6313f5a40e24bb12b543837b7e2
chisommuorah <muorahmarypeace@gmail.com>
2021-10-27 15:55:30 +0100

    still on mobile

--- ecommerce-product/src/components/header/Header.js
+++ ecommerce-product/src/components/header/Header.js
@@ -1,16 +1,30 @@
 import React from "react";
+
 import imatar from "../../images/image-product-1.jpg";
+import previous from "../../images/icon-previous.svg";
+import next from "../../images/icon-next.svg";
+
+import "./header.css";
 
 const Header = () => {
   return (
     <div>
-      <img
-        src={imatar}
-        alt=""
-        style={{
-          maxWidth: "375px",
-        }}
-      />
+      <div className="carousel">
+        <img
+          src={imatar}
+          alt=""
+          style={{
+            maxWidth: "375px",
+          }}
+        />
+      </div>
+
+      <button className="previous">
+        <img src={previous} alt="" />
+      </button>
+      <button className="next">
+        <img src={next} alt="" />
+      </button>
     </div>
   );
 };
