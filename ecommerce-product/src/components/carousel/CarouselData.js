4c8b3a21d9b63f0fa135fa126d6641d0fa68044d
chisommuorah <muorahmarypeace@gmail.com>
2021-10-30 21:37:43 +0100

    added thumbnail data

--- ecommerce-product/src/components/carousel/CarouselData.js
+++ ecommerce-product/src/components/carousel/CarouselData.js
@@ -3,9 +3,16 @@ import image2 from "../../images/image-product-2.jpg";
 import image3 from "../../images/image-product-3.jpg";
 import image4 from "../../images/image-product-4.jpg";
 
+// thumbnails
+import thumbnail1 from "../../images/image-product-1-thumbnail.jpg";
+import thumbnail2 from "../../images/image-product-2-thumbnail.jpg";
+import thumbnail3 from "../../images/image-product-3-thumbnail.jpg";
+import thumbnail4 from "../../images/image-product-4-thumbnail.jpg";
+
 export const CarouselData = [
   {
     image: image1,
+    thumbnail: thumbnail1,
   },
   {
     image: image2,
