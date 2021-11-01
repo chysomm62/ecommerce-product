5f552561867aa2bd6773941cf1c289d7d7baa1a8
chisommuorah <muorahmarypeace@gmail.com>
2021-11-01 16:52:10 +0100

    some styling

--- ecommerce-product/src/components/header/Header.js
+++ ecommerce-product/src/components/header/Header.js
@@ -3,13 +3,11 @@ import React from "react";
 import "./header.css";
 import Carousel from "../carousel/Carousel";
 import { CarouselData } from "../carousel/CarouselData";
-import CartModal from "../modals/CartModal";
 
 const Header = () => {
   return (
     <div>
       <Carousel slides={CarouselData} />
-      <CartModal />
     </div>
   );
 };
