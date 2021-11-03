6221bc78c54c93630d90e316835d0242ed1b823e
chisommuorah <muorahmarypeace@gmail.com>
2021-11-03 13:03:15 +0100

    build

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
+    <BrowserRouter basename={window.location.pathname || ""}>
+      <Route exact path="/" component={App} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
