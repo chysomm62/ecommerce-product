188c4536c3fe2438a58f0be0d3aa1303d07f5f19
chisommuorah <muorahmarypeace@gmail.com>
2021-11-03 14:40:15 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/index.html">
+    <Router>
       <div className="App">
         <Nav />
         <Switch>
